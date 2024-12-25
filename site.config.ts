c6dc6327ce6d8a63a59ddcf14ad224b4a87bc478
Junkai Ji <98195504+swampholyten@users.noreply.github.com>
2024-12-26 00:44:02 +0100

    disable search

--- site.config.ts
+++ site.config.ts
@@ -49,7 +49,6 @@ export default siteConfig({
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
   pageUrlOverrides: null,
-  isSearchEnabled: true,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
